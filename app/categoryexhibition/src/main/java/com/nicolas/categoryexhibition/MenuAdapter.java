d537232388f43528fc7eeef20e9fcf9a8a5f56e4
nicolas <fshqss@live.com>
2020-06-17 18:47:43 +0800

    Initial commit

---
+++ app/categoryexhibition/src/main/java/com/nicolas/categoryexhibition/MenuAdapter.java
@@ -0,0 +1,71 @@
+package com.nicolas.categoryexhibition;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MenuAdapter extends BaseAdapter {
+
+    private Context context;
+    private int selectItem = 0;
+    private List<String> list;
+
+    public MenuAdapter(Context context, List<String> list) {
+        this.list = list;
+        this.context = context;
+    }
+
+    public int getSelectItem() {
+        return selectItem;
+    }
+
+    public void setSelectItem(int selectItem) {
+        this.selectItem = selectItem;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int arg0) {
+        return list.get(arg0);
+    }
+
+    @Override
+    public long getItemId(int arg0) {
+        return arg0;
+    }
+
+    @Override
+    public View getView(int arg0, View arg1, ViewGroup arg2) {
+        ViewHolder holder = null;
+        if (arg1 == null) {
+            holder = new ViewHolder();
+            arg1 = View.inflate(context, R.layout.item_menu, null);
+            holder.tv_name = (TextView) arg1.findViewById(R.id.item_name);
+            arg1.setTag(holder);
+        } else {
+            holder = (ViewHolder) arg1.getTag();
+        }
+        if (arg0 == selectItem) {
+            holder.tv_name.setBackgroundColor(Color.WHITE);
+            holder.tv_name.setTextColor(Color.GREEN);
+        } else {
+            holder.tv_name.setBackgroundColor(Color.GREEN);
+            holder.tv_name.setTextColor(Color.BLACK);
+        }
+        holder.tv_name.setText(list.get(arg0));
+        return arg1;
+    }
+
+    static class ViewHolder {
+        private TextView tv_name;
+    }
+}
